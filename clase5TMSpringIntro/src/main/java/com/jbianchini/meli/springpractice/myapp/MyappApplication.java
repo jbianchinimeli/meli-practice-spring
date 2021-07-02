daca75330be0003cd0176adf74d2568cd3ea5736
jbianchini <juan.bianchini@mercadolibre.com>
2021-07-02 12:11:30 -0300

    Initializing Spring project

---
+++ clase5TMSpringIntro/src/main/java/com/jbianchini/meli/springpractice/myapp/MyappApplication.java
@@ -0,0 +1,13 @@
+package com.jbianchini.meli.springpractice.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyappApplication.class, args);
+    }
+
+}
---
+++ clase5TMSpringIntro/src/test/java/com/jbianchini/meli/springpractice/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbianchini.meli.springpractice.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
